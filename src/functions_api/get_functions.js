0257e75e1d1beaf11a21ecabfdb845f3e493cafe
Etneciv <etneciv009@gmail.com>
2025-02-28 01:44:08 -0600

    GetCityById, get Only 5 pronostic weather days and resturn

--- src/functions_api/get_functions.js
+++ src/functions_api/get_functions.js
@@ -14,3 +14,17 @@ export  async function GetCitiesList() {
         return err;
     }
 }
+
+export async function GetSpecificCity(id) {
+    try{
+        const response = await fetch(`${base_url}citySpecific/${id}`,{
+            headers:{},
+            cache:'no-cache'
+            
+        })
+        return await response.json()
+    }catch(err){
+        console.log(err)
+        return err;
+    }
+}
--- src/pages/city/[id].js
+++ src/pages/city/[id].js
@@ -1,11 +1,31 @@
 'use client';
 import '../../app/globals.css'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useRouter } from 'next/router';
 import ContentPage from '@/ContentPage'
+import { GetSpecificCity } from '@/functions_api/get_functions';
 export default function SpecifcPage(){
+  const [city, setCity] = useState();
+
+  //QUEDA PENDIENTE DESARROLLAR ESTA PÁGINA EN FRONTEND
+  const router = useRouter();
+  const { id } = router.query; // Utiliza destructuring para acceder al parámetro 'id'
+
+  useEffect(() => {
+    async function fetchData() {
+      if (id) {
+        const result = await GetSpecificCity(id);
+        setCity(result);
+        console.log(result);
+        return result;
+      }
+    }
+    fetchData();
+  }, [id]);
+
   return (
     <ContentPage>
-      Hola Mundo
+      Hola Mundoss
     </ContentPage>
   )
 }
